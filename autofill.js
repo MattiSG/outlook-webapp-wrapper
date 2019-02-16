bbbd12eaf40217e8a35585fd0ebd471538f5448a
Matti Schneider <meae@matti.consulting>
2019-02-16 23:37:51 +0100

    Load token information and log in

--- autofill.js
+++ autofill.js
@@ -1,2 +1,3 @@
 document.querySelector('input[name="username"]').value = 'schneiderm'
-document.querySelector('input[name="password"]').focus()
+document.querySelector('input[name="password"]').value = tokenValue
+document.querySelector('input[type="submit"]').click()
--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const { app, BrowserWindow } = require('electron')
 
 
 app.once('ready', () => {
-    const browser = new BrowserWindow()
-    browser.loadURL('https://portalis.diplomatie.gouv.fr/')
+    const token = new BrowserWindow({ width: 500, height: 200 })
+    token.loadURL(`file://${__dirname}/token.html`)
 
-    require('fs').readFile('./autofill.js', 'utf8', (err, autofillScript) => {
+    const webmail = new BrowserWindow({ show: false })
+    webmail.loadURL('https://portalis.diplomatie.gouv.fr/')
+
+    let autofillScript = ''
+
+    require('fs').readFile('./autofill.js', 'utf8', (err, contents) => {
         if (err) return console.error(err)
+        autofillScript = contents
+    })
 
-        browser.webContents.executeJavaScript(autofillScript)
+    token.on('page-title-updated', (event, tokenValue) => {
+        if (tokenValue == 'Token') return 'initial load';
+        token.close()
+        webmail.show()
+        webmail.webContents.executeJavaScript(`tokenValue = ${tokenValue}`)
+        webmail.webContents.executeJavaScript(autofillScript)
     })
 })
 
