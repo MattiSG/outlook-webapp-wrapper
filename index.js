b25d457729005742e6c77ef0531b06b119058cc8
Matti Schneider <meae@matti.consulting>
2019-02-21 11:30:54 +0100

    Log in to webmail

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require('electron')
 
 const USERNAME = 'schneiderm'
+const PASSWORD = 'HERE GOES YOUR PASSWORD'
 
 
 app.once('ready', () => {
@@ -18,18 +19,47 @@ app.once('ready', () => {
     })
 
     webmail.on('page-title-updated', (event, title) => {
-        if (title == 'F5 Dynamic Webtop')
+        if (title == 'F5 Dynamic Webtop') {  // first login successful, select the app to use
             selectWebmailIcon(webmail)
+                .then(getWebmailWindow)
+                .then(webmailWindow => {
+                    webmailWindow.on('page-title-updated', (event, title) => {
+                        if (title == 'Outlook Web App')
+                            autofillInnerLogin(webmailWindow, PASSWORD)
+                    })
+                })
+        }
     })
 })
 
 
 function autofillOuterLogin(view, password) {
+    console.log('Outer login')
+
     view.webContents.executeJavaScript(`document.querySelector("input[name=username]").value = "${USERNAME}"`)
     view.webContents.executeJavaScript(`document.querySelector("input[name=password]").value = "${password}"`)
     view.webContents.executeJavaScript('document.querySelector("input[type=submit]").click()')
 }
 
 function selectWebmailIcon(view) {
-    view.webContents.executeJavaScript('document.querySelector("[class=image]").click()')
+    console.log('Choosing webmail app')
+
+    return view.webContents.executeJavaScript('document.querySelector("[class=image]").click()')
+}
+
+function getWebmailWindow() {
+    console.log('Identifying webmail window')
+
+    return BrowserWindow.getAllWindows().find(window => {
+        return window.getTitle() == '_blank'
+    })
+}
+
+function autofillInnerLogin(view, password) {
+    console.log('Inner login')
+
+    view.webContents.executeJavaScript('document.getElementById("rdoPrvt").click()')
+    view.webContents.executeJavaScript(`document.getElementById("username").value = "${USERNAME}"`)
+    view.webContents.executeJavaScript(`document.getElementById("password").value = "${password}"`)
+    view.webContents.executeJavaScript('document.querySelector("input[type=submit]").click()')
 }
